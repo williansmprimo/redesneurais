28d4eb62a3adc791c7076d368b5b8be2e492e991
Willians Magalhães Primo <williansmprimo@gmail.com>
2023-06-18 16:33:50 -0300

    first commit

---
+++ Anotaçôes Transformer TensorFlow.py
@@ -0,0 +1,20 @@
+# Anotaçôes Transformer TensorFlow
+pip install tensorflow_datasets
+pip install -U tensorflow-text
+
+#imports
+import collections
+import logging
+import os
+import pathlib
+import re
+import string
+import sys
+import time
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow_datasets as tfds
+import tensorflow_text as text
+import tensorflow as tf
